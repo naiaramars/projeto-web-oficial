419ffaafde1794954b7e6092016d3388b41f50e6
naiaramars <128870163+naiaramars@users.noreply.github.com>
2023-07-18 22:10:40 -0300

    Add files via upload

---
+++ js/carrossel.js
@@ -0,0 +1,20 @@
+var splide = new Splide( '#splide01', {
+    perPage: 3,
+    focus  : 0,
+    omitEnd: true,
+} );
+
+splide.mount();
+
+var splide = new Splide( '#splide02', {
+    perPage: 3,
+    focus  : 0,
+    omitEnd: true,
+} );
+
+splide.mount();
+
+// new Splide( '#splide01' ).mount();
+
+// new Splide( '#splide02' ).mount();
+
